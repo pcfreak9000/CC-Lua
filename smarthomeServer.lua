4684a16c4c37ada5e022af1f310d776ac4b1f5fd
pcfreak9000 <roman.borris@gmail.com>
2020-04-16 16:46:21 +0200

    first collider scanning in smarthomeServer

--- smarthomeServer.lua
+++ smarthomeServer.lua
@@ -1,5 +1,7 @@
 --Configuration below
 os.loadAPI("lib/tracking.lua")
+os.loadAPI("lib/collider.lua")
+os.loadAPI("lib/util.lua")
 tracking.setSensorDistance(4)
 tracking.setPositionOfCenter(2831, 94, -255)
 tracking.setSensorLocation("center", "bottom")
@@ -14,18 +16,68 @@ positionsRange = 1000
 --Configuration above
 tracking.initialize()
 
+--key=colName, value=collider
+local registeredColliders = {}
+
+--key=colName, value={key=index, value=player}
+local occupiedColliders = {}
+
+--key=colName, value={key=index, value={prog, ui}}
+local registeredHandlers = {}
+
+
 local function handleCommand(sid, msg, ptc)
     --TODO actions
 end
 
 local function handleRecurring()
-     --TODO scan for the players, do stuff if they violate certain boundaries
+     local players = tracking.getPlayerPositions(positionsRange)
+     --Check if anyone of the players just left any of the colliders. If so, take appropiate action
+     for colName, pArray in pairs(occupiedColliders) do
+        for k,pl in pairs(pArray) do
+            local vec = players[pl]
+            if vec == nil or not registeredColliders[colName]:isInside(vec) then 
+                print("onDeactivate: "..pl)
+                --TODO trigger onDeactivate-event
+                table.remove(pArray, k)
+            end
+        end
+     end
+     --Check if anyone of the players just joined any of the colliders. If so, take appropiate action
+     for k,ve in pairs(players) do
+        for colName, col in pairs(registeredColliders) do
+            local freshJoined = true
+            if occupiedColliders[colName] ~= nil then
+                freshJoined = not util.tableContains(occupiedColliders[colName], k)
+            end
+            if freshJoined and col:isInside(ve) then
+                print("onActivate: "..k)
+                --TODO trigger onActivate-event
+                if occupiedColliders[colName] == nil then
+                    occupiedColliders[colName] = {}
+                end
+                table.insert(occupiedColliders[colName], k)
+            end
+        end
+     end
 end
 
 local function resetTimer(time)
     timer = os.startTimer(time)
 end
- 
+
+function registerHandler(colName, program, uniqueInfo)
+    local data = {prog=program, ui=uniqueInfo}
+    if registeredHandlers[colName] == nil then
+        registeredHandlers[colName] = {}
+    end
+    table.insert(registeredHandlers[colName], data)
+end
+
+function registerCollider(name, collider)
+    registeredColliders[name] = collider
+end
+
 print("Starting smarthome server...")
 resetTimer(0.5)
 rednet.open(rednetSide)
