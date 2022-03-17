51baeaa74c533fd97f87c4dfb97ac0fa27e33014
pcfreak9000 <roman.borris@gmail.com>
2022-03-17 22:04:49 +0100

    stuff

--- home-automation/server.lua
+++ home-automation/server.lua
@@ -5,17 +5,29 @@ local serverModuleFiles = {}
 
 os.loadAPI("cryptoNet")
 os.loadAPI("util")
+os.loadAPI("perms")
 
 local modulesLoaded = {}
-local commandRunners = {}
+local commands = {}
+--local currentModuleName = nil
+
+-- Command stuff
+
+function addCommand(name, runner, plvl) then
+    plvl = plvl or 1
+    commands[name] = {id=currentModuleName, runner=runner, plvl=plvl}
+end
+
+-- Module stuff
 
 function loadModules()
     print("Loading modules...")
     for i,v in ipairs(serverModuleFiles) do
         local ok, ret = pcall(dofile, v)
-        if ok then
-            table.insert(modulesLoaded, ret)
-            print("Loaded module '"..v.."'")
+        if ok and ret.id ~= nil then
+            -- TODO maybe check if the id is valid?
+            modulesLoaded[ret.id] = ret
+            print("Loaded module '"..v.."'/'"..ret.id.."'")
         else
             print("Could not load module '"..v.."'")
         end
@@ -24,29 +36,47 @@ function loadModules()
 end
 
 function setupModules()
-    for i,v in ipairs(modulesLoaded) do
+    local setupfunctionstable = {addCommand=addCommand}
+    for n,v in pairs(modulesLoaded) do
+        currentModuleName = n
         local func = v.setup;
         if func ~= nil then
-            func()
+            func(setupfunctionstable)
         end 
+        currentModuleName = nil
     end
 end
 
 -- Handling event stuff etc below
 
-function handleCommand(socket, cmd)
-    local cmdtab = util.splitArgs(cmd)
-    for i=1, #cmdtab do 
-        print(i .. ": " .. cmdtab[i])
-    end
-    cryptoNet.send(socket, {typ="prompt"})
-    -- check if any module has a cmd 
-end
-
 function sendError(socket, text)
     local tosend = {typ="print", msg=text}
     cryptoNet.send(socket, tosend)
     cryptoNet.send(socket, {typ="prompt"})
+end
+
+function handleCommand(socket, cmd)
+    local cmdtab = util.splitArgs(cmd)
+    -- find command
+    local cmdid = cmdtab[1]
+    local cmdinfo = commands[cmdid]
+    if cmdinfo ~= nil then
+        -- check permissions
+        local user = socket.username
+        local permstring = cmdinfo.id..".command.".cmdid
+        if perms.hasPermission(user, permstring) and socket.permissionLevel >= cmdinfo.plvl then
+            --TODO use pcall
+            local ok, ret = pcall(cmdinfo.runner, cmdtab)
+            if not ok then
+                sendError(socket, "500 Internal Server Error")
+                print("An error occured while executing a command: "..ret)
+            end
+        else
+            sendError(socket, "403 Forbidden")
+        end
+    else
+        sendError(socket, "404 Not Found")
+    end
 end
 
 -- Event stuff below
@@ -62,11 +92,7 @@ function onEvent(event)
             local received = event[2]
             if received.typ ~= nil then
                 if received.typ == "command" then
-                    local ok, errmsg = pcall(handleCommand, socket, received.cmd)
-                    if not ok then
-                        sendError(socket, "500 Internal Server Error")
-                        print("An error occured while handling a command: "..errmsg)
-                    end
+                    handleCommand(socket, received.cmd)
                 end
             else
                 -- a message without type information came in
